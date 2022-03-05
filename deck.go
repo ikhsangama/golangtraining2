f4ec902ee8b2433c1c356ab4ca7252778c0481b1
ikhsangama <ikhsangama@gmail.com>
2022-03-05 11:22:21 +0700

    28

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -39,4 +40,12 @@ func (d deck) toString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) {
+	_, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("error: ", err)
+		os.Exit(1)
+	}
 }
