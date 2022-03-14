61dca0a260d697bac1cf8a1a96eb7fd3082dbd52
ikhsangama <ikhsangama@gmail.com>
2022-03-14 22:36:12 +0700

    47

--- main.go
+++ main.go
@@ -2,35 +2,12 @@ package main
 
 import "fmt"
 
-type contactInfo struct {
-	email   string
-	zipCode int
-}
-
-type person struct {
-	firstName string
-	lastName  string
-	contactInfo
-}
-
 func main() {
-	jim := person{
-		firstName: "Jim",
-		lastName:  "Parse",
-		contactInfo: contactInfo{
-			email:   "jim@gmail.com",
-			zipCode: 1323,
-		},
-	}
-	//jimPointer := &jim
-	jim.updateName("Jimmy")
-	jim.print()
+	mySlice := []string{"Hi", "There"}
+	updateSlice(mySlice)
+	fmt.Println(mySlice)
 }
 
-func (p person) print() {
-	fmt.Printf("%+v", p)
-}
-
-func (pointerToPerson *person) updateName(newFirstName string) {
-	pointerToPerson.firstName = newFirstName
+func updateSlice(s []string) {
+	s[0] = "Bye"
 }
