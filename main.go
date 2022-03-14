2e87bb9a6b52f7cc2e1fc66765356a460056d0f3
ikhsangama <ikhsangama@gmail.com>
2022-03-14 19:04:19 +0700

    40

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	firstWay := person{"ikhsan", "gama"}
+	fmt.Println(firstWay)
 
+	secondWay := person{firstName: "ikhsan", lastName: "gama"}
+	fmt.Println(secondWay)
+
+	var thirdWay person
+	fmt.Println(thirdWay)
+	thirdWay.firstName = "ikhsan"
+	thirdWay.lastName = "gama"
+	fmt.Printf("%+v", thirdWay)
 }
