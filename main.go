3c2735568444b6a2f14ad3b3b276e42946b6018a
ikhsangama <ikhsangama@gmail.com>
2022-03-14 17:25:30 +0700

    assignment1

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-func main() {
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
-}
+import "fmt"
 
-func newCard() string {
-	return "asd"
+func main() {
+	numbers := make([]int, 11)
+	for index, _ := range numbers {
+		if index%2 == 0 {
+			fmt.Printf("%v is even \n", index)
+		} else {
+			fmt.Printf("%v is odd \n", index)
+		}
+	}
+
 }
