d13f79ae2f68dd2f890849472359ef67cbd3c64f
ikhsangama <ikhsangama@gmail.com>
2022-03-15 20:09:43 +0700

    50

--- main.go
+++ main.go
@@ -3,10 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4ba1fi",
-	}
+	// first way
+	//colors := map[string]string{
+	//	"red":   "#ff0000",
+	//	"green": "#4ba1fi",
+	//}
 
+	// second way
+	//var colors map[string]string
+
+	//third way
+	colors := make(map[string]string)
+
+	//add extra key-value pair
+	colors["white"] = "#ffffff"
+
+	fmt.Println(colors)
+	fmt.Println(colors["white"])
+
+	//delete
+	delete(colors, "white")
 	fmt.Println(colors)
 }
