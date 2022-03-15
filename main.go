398b11766718c9f8009f59b6bbcefb1ce791b752
ikhsangama <ikhsangama@gmail.com>
2022-03-15 20:04:21 +0700

    49

--- main.go
+++ main.go
@@ -3,21 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	// there is value types : int float string bool structs
-	// use pointers to change things in func
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4ba1fi",
+	}
 
-	// and there is reference type ( still referencing the same underlying source of data in memory) : slices maps channels pointers functions
-	// don't worry about pointers
-
-	mySlice := []string{"Hi", "There"}
-	fmt.Printf("pointer 1 %p \n", &mySlice)
-	fmt.Printf("pointer 1 %p \n", &mySlice[0])
-	updateSlice(mySlice)
-	fmt.Println(mySlice)
-}
-
-func updateSlice(s []string) {
-	fmt.Printf("pointer 2 %p \n", &s)
-	fmt.Printf("pointer 2 %p \n", &s[0])
-	s[0] = "Bye"
+	fmt.Println(colors)
 }
