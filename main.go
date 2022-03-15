80acf782f974caa9349e31fb53a50f753a7f0bdb
ikhsangama <ikhsangama@gmail.com>
2022-03-15 20:17:08 +0700

    51

--- main.go
+++ main.go
@@ -3,25 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	// first way
-	//colors := map[string]string{
-	//	"red":   "#ff0000",
-	//	"green": "#4ba1fi",
-	//}
-
-	// second way
-	//var colors map[string]string
-
-	//third way
-	colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4ba1fi",
+	}
 
 	//add extra key-value pair
 	colors["white"] = "#ffffff"
 
-	fmt.Println(colors)
-	fmt.Println(colors["white"])
+	printMap(colors)
+}
 
-	//delete
-	delete(colors, "white")
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
