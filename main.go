da3c4e42794fbe20c2de0ac7ecb4b3055e782ca0
ikhsangama <ikhsangama@gmail.com>
2022-03-20 21:08:25 +0700

    58

--- main.go
+++ main.go
@@ -1,40 +1,15 @@
 package main
 
-import "fmt"
-
-type bot interface {
-	// If you are a type in this program with a
-	// function called 'getGreeting' and you return a
-	// string then you are now an honorary member
-	// of type 'bot'
-	getGreeting() string
-}
-
-type englishBot struct {
-}
-type spanishBot struct {
-}
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
-
-	printGreeting(eb)
-	printGreeting(sb)
-}
-
-func printGreeting(b bot) {
-	fmt.Println(b.getGreeting())
-}
-
-// you can omit the value of struct if not using it
-func (englishBot) getGreeting() string {
-	//	CUSTOM LOGIC
-	return "Hi There"
-}
-
-// you can omit the value of struct if not using it
-func (spanishBot) getGreeting() string {
-	//	CUSTOM LOGIC
-	return "Hola"
+	resp, err := http.Get("https://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println(resp)
 }
